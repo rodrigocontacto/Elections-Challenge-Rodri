b671f9e01ebe4af715807658a1ac092330f563f0
= <=>
2020-01-28 11:30:31 -0300

    first commit

---
+++ src/test/java/net/avalith/elections/ElectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.avalith.elections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
